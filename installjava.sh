d30888f60b2a57f96010fb46ec79a171b1e5da95
Artjom <artjom.lell@web.de>
2019-11-03 21:50:53 +0100

    added dialog and config file support.

--- installjava.sh
+++ installjava.sh
@@ -1,36 +1,99 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC2001
+CONFIG_FILE=java-versions.cfg
 
 # Make sure only root can run this script
-if [[ $EUID -ne 0 ]]
-then
-    echo "This script must be run as root" 1>&2
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+# check if config file exist
+if ! [[ -f $CONFIG_FILE ]]; then
+  echo "$CONFIG_FILE can not be found"
+  exit 1
+fi
+
+# check if dialog is installed
+dialog --version >/dev/null 2>&1 || {
+  echo >&2 "Dialog is requiered. Please install dialog and try again"
+  exit 1
+}
+
+CONFIG_REGEX='[A-Za-z0-9]=https://*'
+readarray configLines <$CONFIG_FILE
+options=("${configLines[@]:1}") #removed the 1st element cause of comment
+
+# new associative array for URL to Java name map
+declare -A NAME_TO_URL
+
+# validate file lines
+for ((i = 0; i < ${#options[@]}; i++)); do
+  if ! [[ ${options[$i]} =~ $CONFIG_REGEX ]]; then
+    echo "Incorrect config file content"
     exit 1
+  fi
+done
+
+MENU_SIZE=${#options[@]}
+MENU_ARRAY_SIZE=$((MENU_SIZE * 2))
+
+# some temp vars. yes, I need that
+incr=1
+indexOfOptionsArray=0
+
+# too complicated to comment this part of code below...
+for ((i = 0; $((i < MENU_ARRAY_SIZE)); i++)); do
+  if ! [[ $((incr % 2)) == 0 ]]; then
+    key=${options[$indexOfOptionsArray]//=http*/}
+    value=$(sed 's/^.*https/https/' <<<"${options[$indexOfOptionsArray]}")
+    MENU_ARRAY[$i]=$key
+    NAME_TO_URL[$key]=$value
+  else
+    for ((j = 0; j < MENU_SIZE; j++)); do
+      if [[ $((j == indexOfOptionsArray)) ]]; then
+        MENU_ARRAY[$i]=${NAME_TO_URL[$key]}
+        break
+      fi
+    done
+    indexOfOptionsArray=$((indexOfOptionsArray + 1))
+  fi
+  incr=$((incr + 1))
+done
+
+# menu dialog
+HEIGHT=300
+WIDTH=1000
+CHOICE_HEIGHT=$MENU_SIZE
+BACKTITLE="Java Installer"
+TITLE="Java Installer"
+MENU="Choose one of the following java version:"
+
+KEY=$(dialog --clear \
+  --backtitle "$BACKTITLE" \
+  --title "$TITLE" \
+  --menu "$MENU" \
+  $HEIGHT $WIDTH "$CHOICE_HEIGHT" \
+  "${MENU_ARRAY[@]}" \
+  2>&1 >/dev/tty)
+
+clear
+
+if [ "$KEY" == "" ]; then
+  exit 1
 fi
 
-# Make sure two parameter is given
-if [ $# -ne 2 ]
-then
-    echo "Please enter two parameters"
-    exit 1
-fi
+URL=${NAME_TO_URL[$KEY]}
+NAME_FOR_JAVA=$KEY
 
-if [[ $1 == https:'//'* ]]
-then
-    LINK=$1;
-else
-    echo "wrong parameter";
-    exit 1;
-fi
+# ok, we can start with installation of java
+JAVA_TAR_ARCHIV="/tmp"/$(basename "$URL")
+DESTINATION="/opt/$NAME_FOR_JAVA"
 
-NAME_FOR_JAVA=$2;
-JAVA_TAR_ARCHIV="/tmp"/$(basename "$LINK");
-DESTINATION="/opt/$NAME_FOR_JAVA";
+wget -nc -P "/tmp" "${URL}"
 
-wget -nc -P "/tmp" "${LINK}";
-
-if ! [ -d "$DESTINATION" ]
-then
-    mkdir "$DESTINATION";
+if ! [ -d "$DESTINATION" ]; then
+  mkdir "$DESTINATION"
 fi
 
 tar -xzf "$JAVA_TAR_ARCHIV" -C /"$DESTINATION"/
@@ -47,7 +110,7 @@ update-alternatives --set "java" "$FULL_PATH_TO_JAVA/bin/java"
 update-alternatives --set "javac" "$FULL_PATH_TO_JAVA/bin/javac"
 update-alternatives --set "jar" "$FULL_PATH_TO_JAVA/bin/jar"
 
-echo "-----";
+echo "-----"
 echo "Current Java Version:"
-java -version;
-echo "-----";
+java -version
+echo "-----"
